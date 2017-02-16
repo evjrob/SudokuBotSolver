0a2e9bdd7c8cd2437f90b75fe0c4d6c713c115a0
Everett Robinson <evjrob@gmail.com>
2017-02-15 23:18:24 -0700

    Moved the onGlobalLayoutListener() stuff from onCreateView() to onViewCreated() to separate it from the actual view creation a bit better.

--- app/src/main/java/com/everettsprojects/sudokubotsolver/SudokuBoardFragment.java
+++ app/src/main/java/com/everettsprojects/sudokubotsolver/SudokuBoardFragment.java
@@ -45,64 +45,65 @@ public class SudokuBoardFragment extends Fragment {
 
             }
         }
-
-        sudokuBoard.getViewTreeObserver().addOnGlobalLayoutListener(
-                new ViewTreeObserver.OnGlobalLayoutListener(){
-
-                    @Override
-                    public void onGlobalLayout() {
-
-                        final int MARGIN = 2;
-
-                        int pWidth = sudokuBoard.getWidth();
-                        int pHeight = sudokuBoard.getHeight();
-                        int colCount = sudokuBoard.getColumnCount();
-                        int rowCount = sudokuBoard.getRowCount();
-                        int w = pWidth/colCount;
-                        int h = pHeight/rowCount;
-
-                        for(int yIndex=0; yIndex<rowCount; yIndex++){
-                            for(int xIndex=0; xIndex<colCount; xIndex++){
-                                GridLayout.LayoutParams params =
-                                        (GridLayout.LayoutParams)sudokuCellViews[yIndex*colCount + xIndex].getLayoutParams();
-
-                                params.setGravity(Gravity.LEFT);
-
-                                int topMargin = MARGIN;
-                                int bottomMargin = MARGIN;
-                                int leftMargin = MARGIN;
-                                int rightMargin = MARGIN;
-
-                                if (xIndex == 2 || xIndex == 3 || xIndex == 5 || xIndex == 6) {
-                                    params.width = w - 3*MARGIN;
-                                    if (xIndex == 2 || xIndex == 5) {
-                                        rightMargin = 2 * MARGIN;
-                                    } else {
-                                        leftMargin = 2 * MARGIN;
-                                    }
-                                } else {
-                                    params.width = w - 2*MARGIN;
-                                }
-                                if (yIndex == 2 || yIndex == 3 || yIndex == 5 || yIndex == 6) {
-                                    params.height = h - 3 * MARGIN;
-                                    if (yIndex == 2 || yIndex == 5) {
-                                        bottomMargin = 2 * MARGIN;
-                                    } else {
-                                        topMargin = 2 * MARGIN;
-                                    }
-                                } else {
-                                    params.height = h - 2 * MARGIN;
-                                }
-
-                                params.setMargins(leftMargin, topMargin, rightMargin, bottomMargin);
-                                sudokuCellViews[yIndex*colCount + xIndex].setLayoutParams(params);
-                            }
-                        }
-
-                        // Remove this layout listener once the cells have been resized.
-                        sudokuBoard.getViewTreeObserver().removeGlobalOnLayoutListener(this);
-                    }});
-
         return rootView;
+    }
+
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        sudokuBoard.getViewTreeObserver().addOnGlobalLayoutListener(
+            new ViewTreeObserver.OnGlobalLayoutListener(){
+                @Override
+                public void onGlobalLayout() {
+                    final int MARGIN = 2;
+
+                    int pWidth = sudokuBoard.getWidth();
+                    int pHeight = sudokuBoard.getHeight();
+                    int colCount = sudokuBoard.getColumnCount();
+                    int rowCount = sudokuBoard.getRowCount();
+                    int w = pWidth / colCount;
+                    int h = pHeight / rowCount;
+
+                    for (int yIndex = 0; yIndex < rowCount; yIndex++) {
+                        for (int xIndex = 0; xIndex < colCount; xIndex++) {
+                            GridLayout.LayoutParams params =
+                                    (GridLayout.LayoutParams) sudokuCellViews[yIndex * colCount + xIndex].getLayoutParams();
+
+                            params.setGravity(Gravity.LEFT);
+
+                            int topMargin = MARGIN;
+                            int bottomMargin = MARGIN;
+                            int leftMargin = MARGIN;
+                            int rightMargin = MARGIN;
+
+                            if (xIndex == 2 || xIndex == 3 || xIndex == 5 || xIndex == 6) {
+                                params.width = w - 3 * MARGIN;
+                                if (xIndex == 2 || xIndex == 5) {
+                                    rightMargin = 2 * MARGIN;
+                                } else {
+                                    leftMargin = 2 * MARGIN;
+                                }
+                            } else {
+                                params.width = w - 2 * MARGIN;
+                            }
+                            if (yIndex == 2 || yIndex == 3 || yIndex == 5 || yIndex == 6) {
+                                params.height = h - 3 * MARGIN;
+                                if (yIndex == 2 || yIndex == 5) {
+                                    bottomMargin = 2 * MARGIN;
+                                } else {
+                                    topMargin = 2 * MARGIN;
+                                }
+                            } else {
+                                params.height = h - 2 * MARGIN;
+                            }
+
+                            params.setMargins(leftMargin, topMargin, rightMargin, bottomMargin);
+                            sudokuCellViews[yIndex * colCount + xIndex].setLayoutParams(params);
+                        }
+                    }
+                    // Remove this layout listener once the cells have been resized.
+                   sudokuBoard.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                }
+            });
     }
 }
