126f8ae8881cbce79363fecd3783a699936fb2b6
Everett Robinson <evjrob@gmail.com>
2017-02-16 17:31:49 -0700

    Moved some remaining sudokuCellView formatting code from onCreateView() into onViewCreated. Also updated comments throughout the SudokuBoardFragment class.

--- app/src/main/java/com/everettsprojects/sudokubotsolver/SudokuBoardFragment.java
+++ app/src/main/java/com/everettsprojects/sudokubotsolver/SudokuBoardFragment.java
@@ -9,12 +9,13 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
 import android.widget.GridLayout;
-import android.widget.TextView;
 
 public class SudokuBoardFragment extends Fragment {
 
-    TextView[] sudokuCellViews;
+    // Keep an array of the TextView based SudokuCellViews to populate out board
+    SudokuCellView[] sudokuCellViews;
 
+    // Our sudoku game bord will be based on GridLayout
     GridLayout sudokuBoard;
 
     @Override
@@ -27,22 +28,19 @@ public class SudokuBoardFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_sudoku_board, container, false);
-
-
         sudokuBoard = (GridLayout) rootView.findViewById(R.id.sudoku_board);
 
+        // Instantiate the sudokuCellViews array with enough spce for all the cells in the game board
         int columnCount = sudokuBoard.getColumnCount();
         int rowCount = sudokuBoard.getRowCount();
         sudokuCellViews = new SudokuCellView[rowCount * columnCount];
+
+        // Create each cell and add it to the sudokuBoard GridLayout view
         for (int yIndex = 0; yIndex < rowCount; yIndex++) {
             for (int xIndex = 0; xIndex < columnCount; xIndex++) {
                 SudokuCellView cellView = new SudokuCellView(this.getActivity(), xIndex, yIndex);
                 sudokuCellViews[yIndex * columnCount + xIndex] = cellView;
                 sudokuBoard.addView(cellView);
-                cellView.setGravity(Gravity.CENTER);
-                cellView.setText(Integer.toString(yIndex * columnCount + xIndex));
-                cellView.setBackgroundColor(Color.WHITE);
-
             }
         }
         return rootView;
@@ -51,6 +49,9 @@ public class SudokuBoardFragment extends Fragment {
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
+        // This approach of adding a global layout listener and running onGlobalLayout() once before removing
+        // the listener feels hackish to me, but it was recommended in a couple stack overflow answers as
+        // the best way to resize elements in a GridLayout and it appears to get the job done.
         sudokuBoard.getViewTreeObserver().addOnGlobalLayoutListener(
             new ViewTreeObserver.OnGlobalLayoutListener(){
                 @Override
@@ -59,15 +60,25 @@ public class SudokuBoardFragment extends Fragment {
 
                     int pWidth = sudokuBoard.getWidth();
                     int pHeight = sudokuBoard.getHeight();
-                    int colCount = sudokuBoard.getColumnCount();
+                    int columnCount = sudokuBoard.getColumnCount();
                     int rowCount = sudokuBoard.getRowCount();
-                    int w = pWidth / colCount;
+                    int w = pWidth / columnCount;
                     int h = pHeight / rowCount;
 
                     for (int yIndex = 0; yIndex < rowCount; yIndex++) {
-                        for (int xIndex = 0; xIndex < colCount; xIndex++) {
+                        for (int xIndex = 0; xIndex < columnCount; xIndex++) {
+
+                            // The currently indexed cell of the sudokuBoard
+                            SudokuCellView cellView = sudokuCellViews[yIndex * columnCount + xIndex];
+
+                            // Set some of the formatting on the sudokuCellView
+                            cellView.setGravity(Gravity.CENTER);
+                            cellView.setText(Integer.toString(yIndex * columnCount + xIndex));
+                            cellView.setBackgroundColor(Color.WHITE);
+
+                            // Adjust the layout param of the view including the height and width, and the margins.
                             GridLayout.LayoutParams params =
-                                    (GridLayout.LayoutParams) sudokuCellViews[yIndex * colCount + xIndex].getLayoutParams();
+                                    (GridLayout.LayoutParams) sudokuCellViews[yIndex * columnCount + xIndex].getLayoutParams();
 
                             params.setGravity(Gravity.LEFT);
 
@@ -76,6 +87,9 @@ public class SudokuBoardFragment extends Fragment {
                             int leftMargin = MARGIN;
                             int rightMargin = MARGIN;
 
+                            // The margins are used to draw the lines of the gameboard by exposing the dark background of the GridLayout
+                            // All of this conditional logic down below is used to ensure that the thick lines used to separate one of the
+                            // blocks of 9 from another block are drawn correctly.
                             if (xIndex == 2 || xIndex == 3 || xIndex == 5 || xIndex == 6) {
                                 params.width = w - 3 * MARGIN;
                                 if (xIndex == 2 || xIndex == 5) {
@@ -98,9 +112,10 @@ public class SudokuBoardFragment extends Fragment {
                             }
 
                             params.setMargins(leftMargin, topMargin, rightMargin, bottomMargin);
-                            sudokuCellViews[yIndex * colCount + xIndex].setLayoutParams(params);
+                            sudokuCellViews[yIndex * columnCount + xIndex].setLayoutParams(params);
                         }
                     }
+
                     // Remove this layout listener once the cells have been resized.
                    sudokuBoard.getViewTreeObserver().removeGlobalOnLayoutListener(this);
                 }
