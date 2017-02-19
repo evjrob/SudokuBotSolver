24ee60a4f6e3f09d634ec3de94b35adf0cf6262f
Everett Robinson <evjrob@gmail.com>
2017-02-18 20:36:43 -0700

    Eliminated SudokuBoardFragment and moved the functionality it contained into ManulEntryFragment. Also moved the associated xml in fragment_sudoku_board.xml into fragment_manual_entry.xml to make things simpler.

--- app/src/main/java/com/everettsprojects/sudokubotsolver/ManualEntryFragment.java
+++ app/src/main/java/com/everettsprojects/sudokubotsolver/ManualEntryFragment.java
@@ -1,12 +1,26 @@
 package com.everettsprojects.sudokubotsolver;
 
 import android.app.Fragment;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.ViewTreeObserver;
+import android.widget.GridLayout;
+import android.widget.TextView;
 
 public class ManualEntryFragment extends Fragment {
+
+    // Keep an array of the TextViews to populate out board
+    TextView[] sudokuCellViews;
+
+    // Our sudoku game bord will be based on GridLayout
+    GridLayout sudokuBoard;
+
+    // Selected cell tracks which cell of the game is currently focused.
+    View selectedCell;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -18,6 +32,169 @@ public class ManualEntryFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View rootView = inflater.inflate(R.layout.fragment_manual_entry, container, false);
+        sudokuBoard = (GridLayout) rootView.findViewById(R.id.sudoku_board);
+
+        // Instantiate the sudokuCellViews array with enough spce for all the cells in the game board
+        int columnCount = sudokuBoard.getColumnCount();
+        int rowCount = sudokuBoard.getRowCount();
+        sudokuCellViews = new TextView[rowCount * columnCount];
+
+        // Create each cell and add it to the sudokuBoard GridLayout view then attach a click listener
+        for (int yIndex = 0; yIndex < rowCount; yIndex++) {
+            for (int xIndex = 0; xIndex < columnCount; xIndex++) {
+                TextView sudokuCellView = new TextView(this.getActivity());
+                sudokuCellViews[yIndex * columnCount + xIndex] = sudokuCellView;
+                sudokuBoard.addView(sudokuCellView);
+
+                sudokuCellView.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        toggleSelectedCell(view);
+                        //return true;
+                    }
+                });
+            }
+        }
         return rootView;
+    }
+
+
+    @Override
+    public void onViewCreated(View view, Bundle savedInstanceState) {
+        // This approach of adding a global layout listener and running onGlobalLayout() once before removing
+        // the listener feels hackish to me, but it was recommended in a couple stack overflow answers as
+        // the best way to resize elements in a GridLayout and it appears to get the job done.
+        sudokuBoard.getViewTreeObserver().addOnGlobalLayoutListener(
+                new ViewTreeObserver.OnGlobalLayoutListener(){
+                    @Override
+                    public void onGlobalLayout() {
+                        final int MARGIN = 2;
+
+                        int pWidth = sudokuBoard.getWidth();
+                        int pHeight = sudokuBoard.getHeight();
+                        int columnCount = sudokuBoard.getColumnCount();
+                        int rowCount = sudokuBoard.getRowCount();
+                        int w = pWidth / columnCount;
+                        int h = pHeight / rowCount;
+
+                        for (int yIndex = 0; yIndex < rowCount; yIndex++) {
+                            for (int xIndex = 0; xIndex < columnCount; xIndex++) {
+
+                                // The currently indexed cell of the sudokuBoard
+                                TextView cellView = sudokuCellViews[yIndex * columnCount + xIndex];
+
+                                // Set some of the formatting on the sudokuCellView
+                                cellView.setGravity(Gravity.CENTER);
+                                cellView.setText(Integer.toString(yIndex * columnCount + xIndex));
+                                cellView.setBackgroundColor(Color.WHITE);
+
+                                // Adjust the layout param of the view including the height and width, and the margins.
+                                GridLayout.LayoutParams params =
+                                        (GridLayout.LayoutParams) sudokuCellViews[yIndex * columnCount + xIndex].getLayoutParams();
+
+                                params.setGravity(Gravity.LEFT);
+
+                                int topMargin = MARGIN;
+                                int bottomMargin = MARGIN;
+                                int leftMargin = MARGIN;
+                                int rightMargin = MARGIN;
+
+                                // The margins are used to draw the lines of the gameboard by exposing the dark background of the GridLayout
+                                // All of this conditional logic down below is used to ensure that the thick lines used to separate one of the
+                                // blocks of 9 from another block are drawn correctly.
+                                if (xIndex == 2 || xIndex == 3 || xIndex == 5 || xIndex == 6) {
+                                    params.width = w - 3 * MARGIN;
+                                    if (xIndex == 2 || xIndex == 5) {
+                                        rightMargin = 2 * MARGIN;
+                                    } else {
+                                        leftMargin = 2 * MARGIN;
+                                    }
+                                } else {
+                                    params.width = w - 2 * MARGIN;
+                                }
+                                if (yIndex == 2 || yIndex == 3 || yIndex == 5 || yIndex == 6) {
+                                    params.height = h - 3 * MARGIN;
+                                    if (yIndex == 2 || yIndex == 5) {
+                                        bottomMargin = 2 * MARGIN;
+                                    } else {
+                                        topMargin = 2 * MARGIN;
+                                    }
+                                } else {
+                                    params.height = h - 2 * MARGIN;
+                                }
+
+                                params.setMargins(leftMargin, topMargin, rightMargin, bottomMargin);
+                                sudokuCellViews[yIndex * columnCount + xIndex].setLayoutParams(params);
+                            }
+                        }
+
+                        // Remove this layout listener once the cells have been resized.
+                        sudokuBoard.getViewTreeObserver().removeGlobalOnLayoutListener(this);
+                    }
+                });
+
+        sudokuBoard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                view.clearFocus();
+                view.requestFocus();
+            }
+        });
+
+        sudokuBoard.setOnFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View view, boolean hasFocus) {
+                View viewSelected = getSelectedCell();
+                // if the selected view exists and it lost focus
+                if (viewSelected != null && !viewSelected.hasFocus()) {
+                    // remove it
+                    removeSelectedCell();
+                }
+            }
+        });
+    }
+
+    // toggleSelectedCell allows for a user to select and deselect a given cell of the sudoku game board,
+    // or simultaneously deselect one cell and select a new one
+    public void toggleSelectedCell(View cell) {
+
+        // If the user has clicked on the already selected cell then we just want to deselect it.
+        boolean deselectOnly = false;
+        if (selectedCell == cell) {
+            deselectOnly = true;
+        }
+
+        removeSelectedCell();
+
+        if (!deselectOnly) {
+            setSelectedCell(cell);
+        }
+    }
+
+    public void setSelectedCell(View cell) {
+        selectedCell = cell;
+        if (selectedCell != null) {
+            // shade the selectedCell to show it has focus.
+            selectedCell.setBackgroundColor(Color.LTGRAY);
+        }
+    }
+
+    public View getSelectedCell() {
+        if (selectedCell != null) {
+            return selectedCell;
+        }
+        return null;
+    }
+
+    public void removeSelectedCell() {
+        if (selectedCell != null) {
+            // restore the background of a deslected cell to white
+            selectedCell.setBackgroundColor(Color.WHITE);
+            selectedCell = null;
+        }
+
+        // clear and reset the focus on the sudoku board
+        sudokuBoard.clearFocus();
+        sudokuBoard.requestFocus();
     }
 }
