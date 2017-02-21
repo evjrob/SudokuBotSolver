034353991ee75ddbe77145715821e9f5d2997b58
Everett Robinson <evjrob@gmail.com>
2017-02-20 22:01:29 -0700

    Created a new layout SudokuLayout that extends GridLayout. Successfully moved the onGlobalLayoutListener content that resized the TextViews in the SudokuLayout to its onLayout() method and simplified my ManualEntryFragment in the process.

--- app/src/main/java/com/everettsprojects/sudokubotsolver/ManualEntryFragment.java
+++ app/src/main/java/com/everettsprojects/sudokubotsolver/ManualEntryFragment.java
@@ -3,15 +3,12 @@ package com.everettsprojects.sudokubotsolver;
 import android.app.Fragment;
 import android.graphics.Color;
 import android.graphics.Typeface;
-import android.os.Build;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
 import android.widget.Button;
-import android.widget.GridLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,7 +24,7 @@ public class ManualEntryFragment extends Fragment implements View.OnClickListene
     boolean displaySolved = false;
 
     // Our sudoku game bord will be based on GridLayout
-    GridLayout sudokuBoard;
+    SudokuLayout sudokuBoard;
 
     // Selected cell tracks which cell of the game is currently focused.
     View selectedCell;
@@ -58,7 +55,7 @@ public class ManualEntryFragment extends Fragment implements View.OnClickListene
         View rootView = inflater.inflate(R.layout.fragment_manual_entry, container, false);
 
         // Instantiate the sudokuCellViews array with enough spce for all the cells in the game board
-        sudokuBoard = (GridLayout) rootView.findViewById(R.id.sudoku_board);
+        sudokuBoard = (SudokuLayout) rootView.findViewById(R.id.sudoku_board);
         int columnCount = sudokuBoard.getColumnCount();
         int rowCount = sudokuBoard.getRowCount();
 
@@ -91,6 +88,10 @@ public class ManualEntryFragment extends Fragment implements View.OnClickListene
                 // in the displaySolved State.
                 sudokuCellView.setBackgroundColor(Color.WHITE);
                 sudokuCellView.setTextColor(Color.DKGRAY);
+                sudokuCellView.setGravity(Gravity.CENTER);
+                sudokuCellView.setTextSize(22);
+                sudokuCellView.setWidth(sudokuCellView.getWidth());
+                sudokuCellView.setHeight(sudokuCellView.getHeight());
 
                 // If we had a saved state then restore those values.
                 if (useSavedState) {
@@ -159,96 +160,6 @@ public class ManualEntryFragment extends Fragment implements View.OnClickListene
 
     @Override
     public void onViewCreated(View view, Bundle savedInstanceState) {
-        /* This approach of adding a global layout listener and running onGlobalLayout() once before removing
-         * the listener appears to be the best way to achieve the resizing of the sudokuBoard's child cells.
-         * I tried to create a custom layout that extended GridLayout and do the resizing in the onLayout()
-         * method, but that led to a huge number of warnings about performance issues from a recursive calling
-         * of the requestLayout function during a layout pass. Setting LayoutParams on the TextViews must
-         * cause requestLayout() to be called at some point.
-         */
-        sudokuBoard.getViewTreeObserver().addOnGlobalLayoutListener(
-                new ViewTreeObserver.OnGlobalLayoutListener(){
-                    @Override
-                    public void onGlobalLayout() {
-                        final int MARGIN = 2;
-
-                        int pWidth = sudokuBoard.getWidth();
-                        int pHeight = sudokuBoard.getHeight();
-                        int columnCount = sudokuBoard.getColumnCount();
-                        int rowCount = sudokuBoard.getRowCount();
-                        int w = pWidth / columnCount;
-                        int h = pHeight / rowCount;
-
-                        for (int yIndex = 0; yIndex < rowCount; yIndex++) {
-                            for (int xIndex = 0; xIndex < columnCount; xIndex++) {
-
-                                int flattenedIndex = yIndex * columnCount + xIndex;
-
-                                // The currently indexed cell of the sudokuBoard
-                                TextView cell = sudokuCellViews.get(flattenedIndex);
-
-                                // Set some of the formatting on the sudokuCellView
-                                cell.setGravity(Gravity.CENTER);
-
-                                // Adjust the layout param of the view including the height and width, and the margins.
-                                GridLayout.LayoutParams params =
-                                        (GridLayout.LayoutParams) cell.getLayoutParams();
-
-                                params.setGravity(Gravity.START);
-                                params.columnSpec = GridLayout.spec(xIndex);
-                                params.rowSpec = GridLayout.spec(yIndex);
-
-                                int topMargin = MARGIN;
-                                int bottomMargin = MARGIN;
-                                int leftMargin = MARGIN;
-                                int rightMargin = MARGIN;
-
-                                // The margins are used to draw the lines of the gameboard by exposing the dark background of the GridLayout
-                                // All of this conditional logic down below is used to ensure that the thick lines used to separate one of the
-                                // blocks of 9 from another block are drawn correctly.
-                                if (xIndex == 2 || xIndex == 3 || xIndex == 5 || xIndex == 6) {
-                                    params.width = w - 3 * MARGIN;
-                                    if (xIndex == 2 || xIndex == 5) {
-                                        rightMargin = 2 * MARGIN;
-                                    } else {
-                                        leftMargin = 2 * MARGIN;
-                                    }
-                                } else {
-                                    params.width = w - 2 * MARGIN;
-                                }
-                                if (yIndex == 2 || yIndex == 3 || yIndex == 5 || yIndex == 6) {
-                                    params.height = h - 3 * MARGIN;
-                                    if (yIndex == 2 || yIndex == 5) {
-                                        bottomMargin = 2 * MARGIN;
-                                    } else {
-                                        topMargin = 2 * MARGIN;
-                                    }
-                                } else {
-                                    params.height = h - 2 * MARGIN;
-                                }
-
-                                params.setMargins(leftMargin, topMargin, rightMargin, bottomMargin);
-                                cell.setLayoutParams(params);
-                            }
-                        }
-
-                        // Remove this layout listener once the cells have been resized.
-                        if (Build.VERSION.SDK_INT < Build.VERSION_CODES.JELLY_BEAN) {
-                            sudokuBoard.getViewTreeObserver().removeGlobalOnLayoutListener(this);
-                        } else {
-                            sudokuBoard.getViewTreeObserver().removeOnGlobalLayoutListener(this);
-                        }
-                    }
-                });
-
-        sudokuBoard.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                view.clearFocus();
-                view.requestFocus();
-            }
-        });
-
         sudokuBoard.setOnFocusChangeListener(new View.OnFocusChangeListener() {
             @Override
             public void onFocusChange(View view, boolean hasFocus) {
---
+++ app/src/main/java/com/everettsprojects/sudokubotsolver/SudokuLayout.java
@@ -0,0 +1,78 @@
+package com.everettsprojects.sudokubotsolver;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.View;
+import android.widget.GridLayout;
+
+
+public class SudokuLayout extends GridLayout implements View.OnClickListener{
+
+    public SudokuLayout(Context context) {
+        super(context);
+        init();
+    }
+
+    public SudokuLayout(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public SudokuLayout(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    // handle focus and click states
+    public void init() {
+        setFocusable(true);
+        setFocusableInTouchMode(true);
+        setOnClickListener(this);
+    }
+
+    // when positioning all children within this
+    // layout, add their focusable state
+    @Override
+    protected void onLayout(boolean c, int l, int t, int r, int b) {
+        super.onLayout(c, l, t, r, b);
+
+        final int MARGIN = 2;
+
+        int pWidth = this.getWidth();
+        int pHeight = this.getHeight();
+        int columnCount = this.getColumnCount();
+        int rowCount = this.getRowCount();
+        int w = (pWidth - (16 * MARGIN))/ columnCount;
+        int h = (pHeight - (16 * MARGIN))/ rowCount;
+
+        int leftoverWidth = pWidth - (columnCount * w + 16 * MARGIN);
+        int leftoverHeight = pHeight - (getRowCount() * h + 16 * MARGIN);
+
+        for (int yIndex = 0; yIndex < rowCount; yIndex++) {
+            for (int xIndex = 0; xIndex < columnCount; xIndex++) {
+
+                int flattenedIndex = yIndex * columnCount + xIndex;
+
+                // The currently indexed cell of the sudokuBoard
+                View cell = this.getChildAt(flattenedIndex);
+
+                int childL = leftoverWidth + MARGIN + (xIndex * (MARGIN + w) + 3 * (xIndex / 3));
+                int childR = leftoverWidth + ((xIndex + 1) * (MARGIN + w) + 3 * (xIndex / 3));
+
+                int childT = leftoverHeight + MARGIN + (yIndex * (MARGIN + h) + 3 * (yIndex / 3));
+                int childB = leftoverHeight + ((yIndex + 1) * (MARGIN + h) + 3 * (yIndex / 3));
+                cell.layout(childL, childT, childR, childB);
+            }
+        }
+    }
+
+    // handle the click events
+    @Override
+    public void onClick(View view) {
+        // clear and set the focus on this viewgroup
+        this.clearFocus();
+        this.requestFocus();
+        // now, the focus listener in Activity will handle
+        // the focus change state when this layout is clicked
+    }
+}
